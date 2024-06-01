86c6e3b2d83cf369b331f611fef9313c1f43ca9f
SHOBHIT SHRIVASTAVA <111689849+Shobhit51@users.noreply.github.com>
2024-06-01 22:41:40 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-{/*         <MyComponent /> */}
+         <MyComponent /> 
         hello
       </header>
     </div>
